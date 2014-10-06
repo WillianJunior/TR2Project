9c3e92cce3cc3426a0e33c1ff21c02307748ae24
Willian Junior <guns945@gmail.com>
2014-10-06 01:19:22 -0300

    get file funct tested and working when file is local

--- src/dfs_server.erl
+++ src/dfs_server.erl
@@ -1,7 +1,7 @@
 -module(dfs_server).
 -behaviour (gen_server).
 -export([start_link/1, start_link/2]).
--export([ping_all/0, hail_all/0, create_file/2, flush_data/0, flush_state/0]).
+-export([ping_all/0, hail_all/0, create_file/2, get_file/1, flush_data/0, flush_state/0]).
 -export([init/1, code_change/3, terminate/2, handle_cast/2
 	, handle_call/3, handle_info/2]).
 -export([get_server_name/1]).
@@ -28,6 +28,9 @@ start_link(Server_Name, Group) ->
 %% Sync call
 create_file(Filename, File) ->
 	gen_server:call({global, get_server_name(node())}, {new_local_file, Filename, File}).
+
+get_file(Filename) ->
+	gen_server:call({global, get_server_name(node())}, {get_file, Filename}).
 
 flush_data() ->
 	gen_server:call({global, get_server_name(node())}, flush_data).
@@ -71,6 +74,32 @@ handle_call({new_file, Filename}, _From, {Servers, Data}) ->
 	New_Data = lists:keymerge(1, Data, [{Filename, false, {}}]),
 	{reply, ok, {Servers, New_Data}};
 
+handle_call({get_file, Filename}, _From, {Servers, Data}) ->
+	File_Found = lists:keyfind(Filename, 1, Data),
+	{New_Data, Return_File} = case File_Found of
+		false ->
+			{Data, doesnt_exist};
+		{_Filename_Found, Here, {File, _Consist, _Versions}} when Here -> 
+			% need to check consist
+			{Data, File};
+		{_Filename_Found, Here, {_File, _Consist, _Versions}} when not Here ->
+			% get remote file field
+			{Remote_File, _Consist, Remote_Versions} = get_remote_file(Filename, Servers),
+			% update the versions list locally
+			New_Versions = lists:keymerge(1, Remote_Versions, [{get_server_name(node()), 1}]),
+			New_D = lists:keyreplace(Filename, 1, Data, {Filename, true, {Remote_File, true, New_Versions}}),
+			% multicast the new holder of file to current holders
+			%Current_Holders = lists:map(fun fst/1, Remote_Versions),
+				%%%% RACE CONDItION!!!!! if 2 new holders: the current holder will send obsolete 
+				%% Versions and the new holders won't know about each other
+				%% Solution: Broadcast instead...
+				%% worse eficiency, but correct
+			reliable_multicast({new_file_holder, Filename, get_server_name(node())}, Servers),
+			{New_D, Remote_File};
+		_ -> {Data, false}
+	end,
+	{reply, Return_File, {Servers, New_Data}};
+
 handle_call(_Reason, _From, State) ->
 	{noreply, State}.
 
@@ -79,6 +108,14 @@ handle_info(_Request, State) ->
 	{noreply, State}.
 
 % Async call
+handle_cast({new_file_holder, Filename, New_Holder}, {Servers, Data}) ->
+	{_Filename_Found, Here, {File, _Consist, Versions}} = lists:keyfind(Filename, 1, Data),
+	if
+		Here ->
+			New_Versions = lists:keymerge(1, Versions, [{New_Holder, 1}])
+
+	end;
+
 handle_cast({hello, From}, {Servers, Data}) ->
 	io:format("hello from ~s~n", [atom_to_list(From)]),
 	Exists = lists:member(From, Servers),
@@ -96,6 +133,8 @@ handle_cast(ping, State) ->
 
 
 %%% Private Functions
+fst({First, Second}) -> First.
+
 hail_all() ->
 	Nodes = nodes(),
 	hail_group(Nodes).
@@ -131,3 +170,5 @@ reliable_multicast(Message, [Dest|Group]) ->
 	reliable_multicast(Message, Group);
 
 reliable_multicast(_Message, []) -> ok.
+
+get_remote_file(Filename, Servers) -> ok.
