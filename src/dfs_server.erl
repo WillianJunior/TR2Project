38d53c2bc776188cfa6a029a60f6903089403c76
Willian Junior <guns945@gmail.com>
2014-10-06 14:01:10 -0300

    get_file working when file is or isn't on cache. tested. need to solve consistency problems and race condition of file_query-file_download

--- src/dfs_server.erl
+++ src/dfs_server.erl
@@ -27,16 +27,16 @@ start_link(Server_Name, Group) ->
 
 %% Sync call
 create_file(Filename, File) ->
-	gen_server:call({global, get_server_name(node())}, {new_local_file, Filename, File}).
+	gen_server:call({global, get_self()}, {new_local_file, Filename, File}).
 
 get_file(Filename) ->
-	gen_server:call({global, get_server_name(node())}, {get_file, Filename}).
+	gen_server:call({global, get_self()}, {get_file, Filename}).
 
 flush_data() ->
-	gen_server:call({global, get_server_name(node())}, flush_data).
+	gen_server:call({global, get_self()}, flush_data).
 
 flush_state() ->
-	gen_server:call({global, get_server_name(node())}, flush_state).
+	gen_server:call({global, get_self()}, flush_state).
 
 %% Async call
 ping_all() ->
@@ -59,7 +59,7 @@ terminate(_Reason, _State) -> ok.
 %% handlers
 % New file sync call
 handle_call({new_local_file, Filename, File}, _From, {Servers, Data}) ->
-	New_Data = {Filename, true, {File, true, [{get_server_name(node()), 1}]}},
+	New_Data = {Filename, true, {File, true, [{get_self(), 1}]}},
 	reliable_multicast({new_file, Filename}, Servers),
 	{reply, ok, {Servers, lists:keymerge(1, Data, [New_Data])}};
 
@@ -76,17 +76,18 @@ handle_call({new_file, Filename}, _From, {Servers, Data}) ->
 
 handle_call({get_file, Filename}, _From, {Servers, Data}) ->
 	File_Found = lists:keyfind(Filename, 1, Data),
+	%atom_to_list(File_Found),
 	{New_Data, Return_File} = case File_Found of
 		false ->
 			{Data, doesnt_exist};
 		{_Filename_Found, Here, {File, _Consist, _Versions}} when Here -> 
 			% need to check consist
 			{Data, File};
-		{_Filename_Found, Here, {_File, _Consist, _Versions}} when not Here ->
+		{_Filename_Found, Here, _File} when not Here ->
 			% get remote file field
 			{Remote_File, _Consist, Remote_Versions} = get_remote_file(Filename, Servers),
 			% update the versions list locally
-			New_Versions = lists:keymerge(1, Remote_Versions, [{get_server_name(node()), 1}]),
+			New_Versions = lists:keymerge(1, Remote_Versions, [{get_self(), 1}]),
 			New_D = lists:keyreplace(Filename, 1, Data, {Filename, true, {Remote_File, true, New_Versions}}),
 			% multicast the new holder of file to current holders
 			%Current_Holders = lists:map(fun fst/1, Remote_Versions),
@@ -94,11 +95,40 @@ handle_call({get_file, Filename}, _From, {Servers, Data}) ->
 				%% Versions and the new holders won't know about each other
 				%% Solution: Broadcast instead...
 				%% worse eficiency, but correct
-			reliable_multicast({new_file_holder, Filename, get_server_name(node())}, Servers),
+			reliable_multicast({new_file_holder, Filename, get_self()}, Servers),
 			{New_D, Remote_File};
 		_ -> {Data, false}
 	end,
 	{reply, Return_File, {Servers, New_Data}};
+
+handle_call({new_file_holder, Filename, New_Holder}, _From, {Servers, Data}) ->
+	{_Filename_Found, Here, {File, Consist, Versions}} = lists:keyfind(Filename, 1, Data),
+	if
+		Here ->
+			New_Versions = lists:keymerge(1, Versions, [{New_Holder, 1}]),
+			New_Data = lists:keyreplace(Filename, 1, Data, {Filename, true, {File, Consist, New_Versions}});
+		true ->
+			New_Data = Data
+	end,
+	{reply, ok, {Servers, New_Data}};
+
+handle_call({file_query, Filename}, _From, {Servers, Data}) ->
+	{_Filename_Found, Here, {_File, _Consist, _Versions}} = lists:keyfind(Filename, 1, Data),
+	% need to check consist
+	Reply = if
+		Here ->
+			{available, get_self()};
+		not Here ->
+			unavailable
+	end,
+	{reply, Reply, {Servers, Data}};
+
+
+handle_call({file_download, Filename}, _From, {Servers, Data}) ->
+	{_Filename_Found, _Here, File} = lists:keyfind(Filename, 1, Data),
+	% don't need to check consist here, since it will be done at file_query
+	%%%% RACE CONDITION!!! what if between file_query and file_download the file is deleted?
+	{reply, File, {Servers, Data}};
 
 handle_call(_Reason, _From, State) ->
 	{noreply, State}.
@@ -108,14 +138,6 @@ handle_info(_Request, State) ->
 	{noreply, State}.
 
 % Async call
-handle_cast({new_file_holder, Filename, New_Holder}, {Servers, Data}) ->
-	{_Filename_Found, Here, {File, _Consist, Versions}} = lists:keyfind(Filename, 1, Data),
-	if
-		Here ->
-			New_Versions = lists:keymerge(1, Versions, [{New_Holder, 1}])
-
-	end;
-
 handle_cast({hello, From}, {Servers, Data}) ->
 	io:format("hello from ~s~n", [atom_to_list(From)]),
 	Exists = lists:member(From, Servers),
@@ -133,7 +155,9 @@ handle_cast(ping, State) ->
 
 
 %%% Private Functions
-fst({First, Second}) -> First.
+fst({First, _Second}) -> First.
+
+get_self() -> get_server_name(node()).
 
 hail_all() ->
 	Nodes = nodes(),
@@ -141,7 +165,7 @@ hail_all() ->
 
 hail_group([N|Ns]) ->
 	Server = get_server_name(N),
-	Me = get_server_name(node()),
+	Me = get_self(),
 	io:format("hailing ~s~n", [atom_to_list(Server)]),
 	gen_server:cast({global, Server}, {hello, Me}),
 	hail_group(Ns);
@@ -165,10 +189,30 @@ reliable_multicast(Message, [Dest|Group]) ->
 			ok
 	catch
 		_Error ->
-			gen_server:cast({global, get_server_name(node())}, {unreachable_dest, Dest})
+			gen_server:cast({global, get_self()}, {unreachable_dest, Dest})
 	end,
 	reliable_multicast(Message, Group);
 
 reliable_multicast(_Message, []) -> ok.
 
-get_remote_file(Filename, Servers) -> ok.
+reliable_multicall(Message, [Dest|Group], Fun) ->
+	io:format("multicalling to ~s~n", [atom_to_list(Dest)]),
+	Response = try gen_server:call({global, Dest}, Message, ?TIMEOUT_MULTICAST_MS) of
+		Reply ->
+			Fun(Reply)
+	catch
+		_Error ->
+			gen_server:cast({global, get_self()}, {unreachable_dest, Dest})
+	end,
+	[Response | reliable_multicall(Message, Group, Fun)];
+
+reliable_multicall(_Message, [], _Fun) -> [].
+
+get_remote_file(Filename, Servers) ->
+	Responses = reliable_multicall({file_query, Filename}, Servers, fun get_available/1),
+	First_Server = hd(Responses),
+	gen_server:call({global, First_Server}, {file_download, Filename}).
+
+
+get_available({available, Server}) -> Server;
+get_available(unavailable) -> [].
